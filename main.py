6a29e05180270a64c95930a06e2f97ea8a18d5df
snuhith95 <snuhithbobbala@gmail.com>
2024-09-26 01:51:52 +0530

    routing

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,make_response,Response
 
 app = Flask(__name__,template_folder="./Templates")
 
+@app.errorhandler(Exception)
+def errFunc(e):
+    return f'<h1>Error Occured</h1><h4>{e}</h4>'
+
 @app.route('/')
 def home():
-    return render_template("home.html")
-    # return render_template('index.html')
+    return render_template("form.html")
 
-@app.route('/upload')
+f=[]
+
+@app.route('/upload', methods=['POST'])
 def uploadFile():
-    # print(request.files())
-    return "yet to complete"
+    print("check")
+    print(request.form)
+    f.append(request.files['image'])
+    return render_template("success.html")
+
+@app.route('/download')
+def success():
+    res = Response("heyy", content_type='image/jpeg')
+    res.headers['Content-Disposition'] = 'attachment; filename=your_image.jpg'
+    return res
+
 
 app.run(debug=True)
